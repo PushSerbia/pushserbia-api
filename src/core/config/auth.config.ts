d5d108e9d6b3c097513ab48191ad7ab7965cc037
Dusan Perisic <dullle@live.com>
2025-03-26 18:07:35 +0100

    feat: add callbackUrls and productionLoginPage to AuthConfig for enhanced configuration #11

--- src/core/config/auth.config.ts
+++ src/core/config/auth.config.ts
@@ -1,11 +1,15 @@
 import { registerAs } from '@nestjs/config';
 
 export interface AuthConfig {
+  callbackUrls: string;
+  productionLoginPage: string;
   loginPage: string;
   accountPage: string;
 }
 
 export default registerAs<AuthConfig>('auth', () => ({
+  callbackUrls: process.env.AUTH_CALLBACK_URLS || '',
+  productionLoginPage: process.env.AUTH_PRODUCTION_LOGIN_PAGE || '',
   loginPage: process.env.AUTH_LOGIN_PAGE_URL || '',
   accountPage: process.env.AUTH_ACCOUNT_PAGE_URL || '',
 }));
