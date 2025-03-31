bac52f1892bec83145ad32d25625616ad4f2ea8e
Dusan Perisic <dullle@live.com>
2025-03-31 22:07:48 +0200

    chore: remove unused AuthGuard from Projects and Votes controllers

--- src/modules/votes/votes.controller.ts
+++ src/modules/votes/votes.controller.ts
@@ -1,21 +1,12 @@
-import {
-  Controller,
-  Get,
-  Param,
-  ParseIntPipe,
-  Post,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { VotesService } from './votes.service';
 import { GetUser } from '../../core/decorators/get-user.decorator';
-import { AuthGuard } from '../../core/guards/auth/auth.guard';
 import { User } from '../users/entities/user.entity';
 
 @Controller('votes')
 export class VotesController {
   constructor(private readonly votesService: VotesService) {}
 
-  @UseGuards(AuthGuard)
   @Post(':projectId')
   async voteForProject(
     @Param('projectId', ParseIntPipe) projectId: number,
