c07c0eaf5968d4a5609e7171f29ee34b2d074bad
Dusan Perisic <dullle@live.com>
2025-04-26 17:18:42 +0200

    refactor: RepositoryService for simplicity and consistency #19
    Simplified method implementations by removing redundant error handling and unused methods. Streamlined type definitions and replaced BadRequestException with HttpException for better adherence to HTTP standards.

--- src/core/repository/repository.service.ts
+++ src/core/repository/repository.service.ts
@@ -1,82 +1,40 @@
-import {
-  DeepPartial,
-  FindManyOptions,
-  FindOptionsWhere,
-  InsertResult,
-  Repository,
-} from 'typeorm';
-import { BadRequestException } from '@nestjs/common';
+import { DeepPartial, FindOptionsWhere, Repository } from 'typeorm';
 import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
-export abstract class RepositoryService<T extends { id: string | number }> {
+export abstract class RepositoryService<T extends { id: string }> {
   protected abstract repository: Repository<T>;
 
   async create(entity: DeepPartial<T>): Promise<T> {
-    return this.repository.save(entity).catch((error) => {
-      if (error.code === '23505') {
-        throw new BadRequestException({ message: [error.detail] });
-      }
-      throw error;
-    });
-  }
-
-  async createMany(
-    entities: QueryDeepPartialEntity<T>[],
-  ): Promise<InsertResult> {
-    return this.repository.insert(entities);
+    return this.repository.save(entity);
   }
 
   async findAll(options?: any): Promise<T[]> {
     return this.repository.find(options);
   }
 
-  async findOne(id: string | number): Promise<T> {
-    const entity = await this.repository.findOneBy({
-      id,
-    } as FindOptionsWhere<T>);
-    if (!entity) {
-      throw new BadRequestException(`Entity with id ${id} not found`);
-    }
-    return entity;
+  findById(id: string) {
+    return this.repository.findOneBy({ id } as FindOptionsWhere<T>);
   }
 
-  async findOneBy(query: Partial<T>): Promise<T | null> {
-    return this.repository.findOneBy(query as FindOptionsWhere<T>);
+  findOneBy(query: FindOptionsWhere<T> | FindOptionsWhere<T>[]) {
+    return this.repository.findOneBy(query);
   }
 
   async update(
-    criteria: string | number | Partial<T>,
+    criteria: string | FindOptionsWhere<T>,
     entity: QueryDeepPartialEntity<T>,
-  ): Promise<T> {
-    await this.repository.update(
-      criteria as string | number | FindOptionsWhere<T>,
-      entity,
-    );
-    return typeof criteria === 'string' || typeof criteria === 'number'
-      ? this.findOne(criteria)
-      : this.repository
-          .findOneBy(criteria as FindOptionsWhere<T>)
-          .then((result) => {
-            if (!result) {
-              throw new BadRequestException(`Entity not found`);
-            }
-            return result;
-          });
+  ) {
+    const updated = await this.repository.update(criteria, entity);
+    if (updated.affected === 0) {
+      throw new HttpException('Entity not found', HttpStatus.NOT_FOUND);
+    }
+    return typeof criteria === 'string'
+      ? this.findById(criteria)
+      : this.findOneBy(criteria);
   }
 
-  async remove(criteria: string | number | Partial<T>): Promise<void> {
-    if (typeof criteria === 'string' || typeof criteria === 'number') {
-      await this.repository.delete(criteria);
-    } else {
-      await this.repository.delete(criteria as FindOptionsWhere<T>);
-    }
-  }
-
-  async count(options: FindManyOptions<T> = {}): Promise<number> {
-    try {
-      return await this.repository.count(options);
-    } catch (error) {
-      throw new BadRequestException(`Error counting records: ${error.message}`);
-    }
+  async remove(criteria: string | FindOptionsWhere<T>): Promise<void> {
+    await this.repository.delete(criteria);
   }
 }
